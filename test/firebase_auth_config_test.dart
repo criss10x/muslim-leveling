04a34b4c9c5537a49b565fe0cbdb9a2587127976
criss10x <criss.lucifer@gmail.com>
2026-08-12 07:57:04 +0800

    fix: add Play signing OAuth config

--- test/firebase_auth_config_test.dart
+++ test/firebase_auth_config_test.dart
@@ -5,14 +5,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:muslim_leveling/services/auth_service.dart';
 
 void main() {
-  test('uses the Web OAuth client from google-services.json', () {
+  test('uses the Web OAuth client and Play signing certificate', () {
     final config =
         jsonDecode(File('android/app/google-services.json').readAsStringSync())
             as Map<String, dynamic>;
-    final clients = (config['client'] as List).single as Map;
-    final oauthClients = clients['oauth_client'] as List;
-    final webClient = (oauthClients.single as Map)['client_id'] as String;
+    final client = (config['client'] as List).single as Map;
+    final oauthClients = client['oauth_client'] as List;
+    final webClient = oauthClients.cast<Map>().singleWhere(
+      (oauth) => oauth['client_type'] == 3,
+    );
+    final androidSha1s = oauthClients
+        .cast<Map>()
+        .where((oauth) => oauth['client_type'] == 1)
+        .map((oauth) => oauth['android_info']['certificate_hash'])
+        .toSet();
 
-    expect(AuthService.googleWebClientId, webClient);
+    expect(webClient['client_id'], AuthService.googleWebClientId);
+    expect(
+      androidSha1s,
+      contains('714387b55178b56364101ca4a1e15450e2cd17e8'),
+    );
   });
 }
